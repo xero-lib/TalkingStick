0282d668fff653b2bf74bb420987e491331acb21
xero-lib <jonathankurtz@protonmail.com>
2020-11-24 13:10:40 -0600

    Improved a few things

--- commands/resolveUserID.js
+++ commands/resolveUserID.js
@@ -1,11 +1,12 @@
 import '../prototypes/tempSend.js';
+import util from 'util';
 import { developer } from '../coagulators/configCoagulator.js';
 import { resolveUserID } from '../coagulators/functionCoagulator.js'
 
 export default async function (message, args) {
     if(message.member.id == developer.id) {
         let user = resolveUserID(args);
-        if(user) message.channel.send(`\`\`\`\nUsername: ${user.username}#${user.discriminator},\nUserID: ${user.id},\nAvatarURL: ${user.avatarURL()}\nIsBot?: ${user.bot},\nClient: ${user.client},\nCreatedAt: ${user.createdAt},\nCreatedTimestamp: ${user.createdTimestamp},\nlastMessageObject: ${user.lastMessage},\nflags: ${user.flags.keys},\n\`\`\``);
+        if(user) message.channel.send(`\`\`\`js\n${util.inspect(user)}\n\`\`\``);// message.channel.send(`\`\`\`\nUsername: ${user.username}#${user.discriminator},\nUserID: ${user.id},\nAvatarURL: ${user.avatarURL()}\nIsBot?: ${user.bot},\nClient: ${user.client},\nCreatedAt: ${user.createdAt},\nCreatedTimestamp: ${user.createdTimestamp},\nlastMessageObject: ${user.lastMessage},\nflags: ${user?.flags},\n\`\`\``);
         else{message.tempSend(args, 'resolveUserID returned **`false`**')}
     }
 }
--- listeners/listenerOn/onChannelCreate.js
+++ listeners/listenerOn/onChannelCreate.js
@@ -1,9 +1,7 @@
-import {client} from '../../coagulators/configCoagulator.js';
-
 export default async function onChannelCreate(c) {
     if(c.name) {
         await c.fetch()
-            .then(s => console.log(`Fetched new channel "${s?.name}" in "${client.guilds.cache.get(s.id)?.name}"`))
-            .catch(e => console.log(e, `\nCould not fetch ${e?.name}`));
+            .then(s => console.log(`Fetched new channel "${s?.name}" in "${s?.guild.name}"`))
+            .catch(e => console.log(`Could not fetch ${e?.name} in ${e?.guild.name}:`,e));
     }
 }
