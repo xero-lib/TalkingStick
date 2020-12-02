000b045db558ce62620c5bb135b405e62e5fe0b3
xero-lib <jonathankurtz@protonmail.com>
2020-12-01 18:01:42 -0600

    added cfg

---
+++ config/botConfig.js
@@ -0,0 +1,10 @@
+
+export const
+    botName = 'Talking Stick',
+    // token = 'MzQ3ODQ1OTQ0Mzc4OTE2ODY3.WZYCxw.njfyVh2ZHdh50n-8OOzjBp0tlTs', //testing
+    token = 'NjA5MjMzNjc2NDcxMTA3NTg0.XUzvEw.hwkwMoUreXDDCxbvYAFYqbq-Cgg',  //main
+    defaultPrefix = "//",
+    roles = ['Stick Holder', 'Stick Controller', 'Stick Listener', 'TSLeft'],
+    botPage = 'https://top.gg/bot/609233676471107584',
+    botInv = 'https://discord.com/oauth2/authorize?client_id=609233676471107584&scope=bot&permissions=307260464',
+    botPfp = 'https://cdn.discordapp.com/avatars/609233676471107584/47fb3e1d1751ab8f4fb24e3e64262d09.webp'
