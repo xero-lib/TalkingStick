5a8f3f2e068d7c3da53049b792a7df514045ca3e
xero-lib <jonathankurtz@protonmail.com>
2021-05-08 17:54:01 -0500

    Fixed edit for main.js -> index.js

--- coagulators/configCoagulator.js
+++ coagulators/configCoagulator.js
@@ -1,3 +1,3 @@
 export {token, defaultPrefix, botPfp, botName, botInv, botPage, roles} from '../config/botConfig.js';
-export {client, developer, application} from '../main.js';
+export {client, developer, application} from '../index.js';
 export {default as Discord} from 'discord.js';
