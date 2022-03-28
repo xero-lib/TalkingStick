28db2cdff30b4193f2b2182c3da63960bf7c57da
Steam Drive <jonathankurtz@protonmail.com>
2022-03-28 06:31:30 -0500

    fixed member count

--- listeners/listenerOnce/onceReady.js
+++ listeners/listenerOnce/onceReady.js
@@ -3,6 +3,8 @@ import { date, setPresence } from "../../exports/functionExports.js";
 import chalk from "chalk";
 
 export default async function () {
-    console.log(date(),`Currently in ${chalk.yellow(client.guilds.cache.map(guild => guild.name).length)} servers\n` + date(), chalk.greenBright(`Ready in ${client.guilds.cache.array().length} servers!`));
-    setPresence();
+        let members = 0;
+        client.guilds.cache.forEach((guild) => members += guild.memberCount);
+        console.log(date(),`Currently in ${chalk.yellow(client.guilds.cache.map(guild => guild.name).length)} servers, serving ${members} users\n` + date(), chalk.greenBright(`Ready in ${client.guilds.cache.array().length} servers!`));
+        setPresence();
 }
