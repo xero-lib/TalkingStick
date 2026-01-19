430c4f43061d598d16cc56106d0ee2f284d8a4f3
Thoth <jonathankurtz@protonmail.com>
2026-01-19 04:30:01 -0600

    added botConfig to versioning

---
+++ config/botConfig.ts
@@ -0,0 +1,9 @@
+export const
+    botPfp     = "https://cdn.discordapp.com/avatars/609233676471107584/47fb3e1d1751ab8f4fb24e3e64262d09.webp",
+    botName    = "Talking Stick",
+    botPage    = "https://top.gg/bot/609233676471107584",
+    // devGuildId = "764720953463799838",    // main
+    devGuildId = "347844679074709504",    // testing
+    // clientId   = "609233676471107584",    // main
+    clientId   = "347845944378916867"     // testing
+;
